5a0e6456417a513812d0c678ab36c7645bf13647
Endre Deak <profendre@gmail.com>
2023-12-03 23:35:11 -0800

    Day 04

--- src/main/kotlin/endredeak/aoc2023/Day04.kt
+++ src/main/kotlin/endredeak/aoc2023/Day04.kt
@@ -1,15 +1,31 @@
 package endredeak.aoc2023
 
+import kotlin.math.pow
+
 fun main() {
-    solve("") {
+    solve("Scratchcards") {
         val input = lines
+            .map { line ->
+                line.substringAfter(":")
+                    .trimStart()
+                    .replace("  ", " ")
+                    .replace(" | ", "|")
+                    .split("|").let { (c, n) ->
+                        val ws = c.split(" ").map { it.toInt() }
+                        val ns = n.split(" ").map { it.toInt() }
 
-        part1(-1) {
-            -1
-        }
+                        ws.count { it in ns }
+                    }
+            }
 
-        part2(-1) {
-            -1
+        part1(17803) { input.filter { it > 0 }.sumOf { 2.0.pow(it - 1).toInt() } }
+
+        part2(5554894) {
+            val counts = input.map { 1 }.toMutableList()
+
+            input.indices.forEach { c -> (c + 1..<c + 1 + input[c]).forEach { w -> counts[w] += counts[c] } }
+
+            counts.sum()
         }
     }
 }
