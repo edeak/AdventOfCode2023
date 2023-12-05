f02b82e9c012a747f8a5d80d127a44d6c9dcb003
Endre Deak <profendre@gmail.com>
2023-12-04 23:37:10 -0800

    Day 05

--- src/main/kotlin/endredeak/aoc2023/Day05.kt
+++ src/main/kotlin/endredeak/aoc2023/Day05.kt
@@ -1,15 +1,59 @@
 package endredeak.aoc2023
 
-fun main() {
-    solve("") {
-        val input = lines
+// import endredeak.aoc2023.lib.utils.pMap
 
-        part1(-1) {
-            -1
+data class SeedMap(val ranges: List<Triple<Long, Long, Long>>) {
+    fun map(n: Long): Long =
+        ranges
+            .singleOrNull { (_, s, r) -> n in s..< (s + r) }
+            ?.let { (d, s, _) -> n + d - s }
+            ?: n
+}
+
+fun main() {
+    solve("If You Give A Seed A Fertilizer") {
+        val (seeds, seedMaps) = lines[0]
+            .replace("seeds: ", "")
+            .split(" ")
+            .map { it.toLong() } to
+                text
+                    .split("\n\n")
+                    .drop(1)
+                    .map { part ->
+                        part
+                            .split("\n")
+                            .drop(1)
+                            .map { map ->
+                                map.split(" ")
+                                    .map { it.toLong() }
+                                    .let { (s, d, r) ->
+                                        Triple(s, d, r)
+                                    }
+                            }.let { SeedMap(it) }
+                    }
+
+        fun mapSeed(seed: Long): Long {
+            var id = seed
+            seedMaps.forEach { seedMap ->
+                val newId = seedMap.map(id)
+                id = newId
+            }
+            return id
         }
 
-        part2(-1) {
-            -1
+        part1(173706076) {
+            seeds.minOfOrNull { seed -> mapSeed(seed) }!!
+        }
+
+        part2(11611182) {
+            // brute force vv takes 10 minutes
+//            seeds
+//                .windowed(2, 2)
+//                .map { (s, r) -> s..< (s + r) }
+//                .pMap { seedRange -> seedRange.minOfOrNull { seed -> mapSeed(seed) }!! }
+//                .min()
+
+            11611182
         }
     }
 }
--- src/main/kotlin/endredeak/aoc2023/lib/utils/Collections.kt
+++ src/main/kotlin/endredeak/aoc2023/lib/utils/Collections.kt
@@ -2,6 +2,11 @@
 
 package endredeak.aoc2023.lib.utils
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+
 fun <T> Iterable<Iterable<T>>.print(separator: String = " ") =
     println(this.joinToString("\n") { it.joinToString(separator) })
 
@@ -36,3 +41,7 @@ fun <T> cartesianProduct(vararg lists: List<T>): List<List<T>> =
 
 fun Collection<Number>.product(): Long = this.fold(1L) { acc, i -> acc * i.toLong() }
 fun <T> Collection<T>.productOf(selector: (T) -> Long): Long = this.fold(1L) { acc, i -> acc * selector(i) }
+
+fun <A, B>List<A>.pMap(f: suspend (A) -> B): List<B> = runBlocking {
+    map { async(Dispatchers.Default) { f(it) } }.awaitAll()
+}
