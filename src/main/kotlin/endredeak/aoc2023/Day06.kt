9e7bbfe8ab05365f519d2561e96ad9d3cc87302b
Endre Deak <profendre@gmail.com>
2023-12-06 14:59:13 -0800

    Day 06 - quadratic

--- src/main/kotlin/endredeak/aoc2023/Day06.kt
+++ src/main/kotlin/endredeak/aoc2023/Day06.kt
@@ -2,6 +2,9 @@ package endredeak.aoc2023
 
 import endredeak.aoc2023.lib.utils.productOf
 import endredeak.aoc2023.lib.utils.transpose
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 fun main() {
     solve("Wait For It") {
@@ -9,20 +12,20 @@ fun main() {
 
         part1(138915) {
             input
-                .map { l -> l.split(" ")
-                    .filter { it.isNotBlank() }
-                    .map { it.toLong() }
-                }
+                .map { it.split(" ").filter(String::isNotBlank).map(String::toLong) }
                 .transpose()
-                .productOf { (t, r) -> countWays(t, r).toLong() }
+                .productOf { solve(it[0], it[1]) }
         }
 
         part2(27340847) {
             input
-                .map { it.replace(" ", "").toLong() }
-                .let { (t, r) -> countWays(t, r) }
+                .map { it.filter(Char::isDigit).toLong() }
+                .let { solve(it[0], it[1]) }
         }
     }
 }
 
-private fun countWays(t: Long, r: Long) = (1..<t).count { (t - it) * it > r }
+// solve x^2 - tx - d = 0
+private fun solve(t: Long, r: Long) =
+    (t / 2.0).let { b -> sqrt(b * b - r).let { d -> (ceil(b + d - 1) - floor(b - d + 1) + 1) } }.toLong()
+    // (1..<t).count { (t - it) * it > r }.toLong() // brute-force solution (around 700ms)
