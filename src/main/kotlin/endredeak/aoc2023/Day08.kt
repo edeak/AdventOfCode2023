c8572109da8a6a5d2ddb463aad6c38c11fd59010
Endre Deak <profendre@gmail.com>
2023-12-07 23:07:16 -0800

    Day 08

--- src/main/kotlin/endredeak/aoc2023/Day08.kt
+++ src/main/kotlin/endredeak/aoc2023/Day08.kt
@@ -1,15 +1,31 @@
 package endredeak.aoc2023
 
+import endredeak.aoc2023.lib.utils.lcm
+
+
 fun main() {
-    solve("") {
-        val input = lines
+    solve("Haunted Wasteland") {
+        val input =
+            text
+                .replace("(", "").replace(")", "")
+                .split("\n\n")
+                .let { (i, m) ->
+                    i to m.split("\n")
+                        .associate { it.split(" = ", ", ").let { (k, l, r) -> k to (l to r) } }
+                }
 
-        part1(-1) {
-            -1
-        }
+        fun run(start: (String) -> Boolean, end: (String) -> Boolean) =
+            input.let { (i, m) ->
+                m.keys.filter(start).fold(1L) { acc, n ->
+                    var s = 0
+                    var c = n
+                    while (end(c)) c = m[c]!!.let { if (i[s % i.length] == 'L') it.first else it.second }.also { s++ }
+                    lcm(acc, s.toLong())
+                }
+            }
 
-        part2(-1) {
-            -1
-        }
+        part1(19637) { run({ it == "AAA" }, { !it.endsWith("ZZZ") }) }
+
+        part2(8811050362409) { run({ it.endsWith("A") }, { !it.endsWith("Z") }) }
     }
 }
