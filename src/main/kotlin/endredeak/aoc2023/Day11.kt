2731c901389d5d645d0ea1a1a2e731ffdeb0effd
Endre Deak <profendre@gmail.com>
2023-12-11 00:24:23 -0800

    Day 11

--- src/main/kotlin/endredeak/aoc2023/Day11.kt
+++ src/main/kotlin/endredeak/aoc2023/Day11.kt
@@ -1,15 +1,33 @@
 package endredeak.aoc2023
 
+import kotlin.math.abs
+
 fun main() {
-    solve("") {
+    solve("Cosmic Expansion") {
+        fun Pair<Long, Long>.dist(other: Pair<Long, Long>) = abs(other.first - first) + abs(other.second - second)
+        fun coordinate(c: Int, adds: List<Int>, factor: Long) = c.toLong() + adds.count { c > it } * (factor - 1)
+
         val input = lines
 
-        part1(-1) {
-            -1
-        }
+        val columns = input[0].indices.filter { i -> input.map { s -> s[i] }.all { it == '.' } }
+        val rows = input.indices.filter { i -> input[i].all { it == '.' } }
 
-        part2(-1) {
-            -1
-        }
+        fun calculate(factor: Long) =
+            input.flatMapIndexed { y, line ->
+                line.mapIndexedNotNull { x, c ->
+                    if (c == '#') {
+                        coordinate(x, columns, factor) to coordinate(y, rows, factor)
+                    } else null
+                }
+            }
+                .let {
+                    it
+                        .flatMap { g -> it.map { n -> g to n } }
+                        .sumOf { (g, n) -> g.dist(n) } / 2
+                }
+
+        part1(9693756) { calculate(2) }
+
+        part2(717878258016) { calculate(1000000) }
     }
 }
