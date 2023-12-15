92c684c42c8934e83ded7881da340222ff11e338
Endre Deak <profendre@gmail.com>
2023-12-14 22:41:05 -0800

    Day 15

--- src/main/kotlin/endredeak/aoc2023/Day15.kt
+++ src/main/kotlin/endredeak/aoc2023/Day15.kt
@@ -1,15 +1,51 @@
 package endredeak.aoc2023
 
 fun main() {
-    solve("") {
-        val input = lines
+    solve("Lens Library") {
+        val input = text.replace("\n", "").split(",")
 
-        part1(-1) {
-            -1
+        fun String.hash(): Int {
+            var current = 0
+            forEach { c ->
+                current += c.code
+                current *= 17
+                current %= 256
+            }
+            return current
         }
 
-        part2(-1) {
-            -1
+        part1(505459) {
+            input.sumOf { it.hash() }
+        }
+
+        data class Lens(val label: String, var focal: Int)
+
+        part2(228508) {
+            val boxes = (0..255).associateWith { mutableListOf<Lens>() }.toMutableMap()
+
+            input.forEach { step ->
+                if (step.contains("-")) {
+                    step.replace("-", "")
+                        .let { label ->
+                            boxes[label.hash()]!!.removeIf { it.label == label }
+                        }
+                }
+
+                if (step.contains("=")) {
+                    step.split("=")
+                        .let { (a, b) -> a to b.toInt() }
+                        .let { (label, focal) ->
+                            boxes[label.hash()]!!
+                                .let { box ->
+                                    box.singleOrNull { it.label == label }
+                                        ?.apply { this.focal = focal }
+                                        ?: box.add(Lens(label, focal))
+                                }
+                        }
+                }
+            }
+
+            boxes.flatMap { (box, lenses) -> lenses.mapIndexed { i, l -> (box + 1) * (i + 1) * l.focal } }.sum()
         }
     }
 }
